b4ab631b09a0dd03a2252dbab0e95aaaa7aa3278
Ayman Amin Ali Amin <ayman1793@hotmail.com>
2022-09-12 21:53:13 +0300

    Gender test

--- src/app/EmployeeManagement/AddEmployee/AddEmployee.component.ts
+++ src/app/EmployeeManagement/AddEmployee/AddEmployee.component.ts
@@ -20,7 +20,7 @@ export class AddEmployeeComponent implements OnInit {
   lb_EmpEmail:any;lb_EmpGender:any;
   lb_EmpSection:any;lb_JobTitle:any;
 
-  GenderList :any = [{"Id":1,"Name":"FeMale"},{"Id":2,"Name":"Male"}];
+  GenderList :any;
   GetLabelName(LangCode:any){
     if(LangCode == "us-en"){
       this.lb_EmpInfo = "Employee Info";
@@ -31,6 +31,7 @@ export class AddEmployeeComponent implements OnInit {
       this.lb_EmpGender = "Gender";
       this.lb_EmpSection = "Section";
       this.lb_JobTitle = "Job Title";
+      this.GenderList = [{"Id":1,"Name":"Female"},{"Id":2,"Name":"Male"}];
     }
     else{
       this.lb_EmpInfo = "بيانات الموظف";
@@ -40,6 +41,7 @@ export class AddEmployeeComponent implements OnInit {
       this.lb_EmpEmail = "البريد الاكتروني";
       this.lb_EmpGender = "الجنس";
       this.lb_EmpSection = "القسم";
+      this.GenderList = [{"Id":1,"Name":"انثى"},{"Id":2,"Name":"ذكر"}];
     }
   }
 
