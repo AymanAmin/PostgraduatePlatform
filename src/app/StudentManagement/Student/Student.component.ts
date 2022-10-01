0ba105553379face3bbd85e1b35a4bce5b0ba7df
Ayman Amin Ali Amin <ayman1793@hotmail.com>
2022-10-01 18:23:30 +0300

    Isinformation

--- src/app/StudentManagement/Student/Student.component.ts
+++ src/app/StudentManagement/Student/Student.component.ts
@@ -11,18 +11,27 @@ export class StudentComponent implements OnInit {
   username: string = "Mazin Awad";
   JobTitle: string = "Computer Science";
 
+  Isinformation:boolean = false;
+
   ngOnInit() {
     this.GetLabelName(this.LangCode);
 
   }
 
   goToDiv(DivID: string) {
-    var div = document.getElementById(DivID);
+    if(DivID == "student-information")
+      this.Isinformation = true;
+    else
+      this. Isinformation = false;
+
+
+
+    /*var div = document.getElementById(DivID);
     div?.scrollIntoView({
       behavior: "smooth",
       block: "start",
       inline: "nearest"
-    });
+    });*/
   }
 
   GetLabelName(LangCode: any) {
